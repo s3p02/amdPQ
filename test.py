ebe5aa4bdfff1a048357234c55ced191608d4f73
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-12-10 12:26:30 -0700

    test.py PEP8 compliant

--- test.py
+++ test.py
@@ -3,13 +3,15 @@ import sys
 import unittest
 from stringManipulation import stringManipulation as encoder
 
+
 class testStringManipulation(unittest.TestCase):
     def testGet(self):
         inputString = "aaeeeeebcdffff"
         resultString = "za2ze5zb1zc1zd1zf4"
         compute = encoder.stringManipulation(inputString).get()
-        self.assertEqual(compute,resultString)
+        self.assertEqual(compute, resultString)
+
 
 if __name__ == "__main__":
-    logging.basicConfig(stream=sys.stdout,level=logging.DEBUG)
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     unittest.main()
