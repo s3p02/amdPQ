263eb404f7936298d307de5f9e466dd23b88a168
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-12-10 12:31:46 -0700

    main.py PEP8 compliant

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import logging
 import sys
 from stringManipulation import stringManipulation as encoder
 
+
 def main():
     inputString = ["aaeeeeebcdffff", "aefrtdddggg", "aertttefze"]
     for str in inputString:
-        print("INPUT: {0} --> OUTPUT: {1}".format(str,encoder.stringManipulation(str).get()))
+        print("INPUT: {0} --> OUTPUT: {1}".format(str, encoder.stringManipulation(str).get()))
+
 
 if __name__ == "__main__":
-    logging.basicConfig(stream=sys.stdout,level=logging.INFO)
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     main()
