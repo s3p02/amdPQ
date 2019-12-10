6da05f49fe23bf3945e216f5aab4b9bf8ab60285
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-12-10 12:37:07 -0700

    charTracker.py PEP8 compliant

--- charTracker/charTracker.py
+++ charTracker/charTracker.py
@@ -1,6 +1,8 @@
 import logging
+
+
 class charTracker:
-    def __init__(self,char,count=0):
+    def __init__(self, char, count=0):
         self.char = char
         logging.debug("CLASS charTracker - METHOD init - char: {}".format(self.char))
         self.count = count
