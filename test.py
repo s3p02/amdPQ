3f46b62ada06353636789f66ce3161e226cfb4f7
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-12-12 00:35:12 -0700

    coverage at 97%

--- test.py
+++ test.py
@@ -9,15 +9,19 @@ class testStringManipulation(unittest.TestCase):
         inputString1 = "aaeeeeebcdffff"
         inputString2 = "aefrtdddggg"
         inputString3 = "aertttefze"
+        inputString4 = "aerttttttttttefze"
         resultString1 = "aaze5bcdzf4"
         resultString2 = "aefrtzd3zg3"
         resultString3 = "aerzt3efze"
+        resultString4 = "aerzt9efze"
         compute1 = encoder.stringManipulation(inputString1).get()
         compute2 = encoder.stringManipulation(inputString2).get()
         compute3 = encoder.stringManipulation(inputString3).get()
+        compute4 = encoder.stringManipulation(inputString4).get()
         self.assertEqual(compute1, resultString1)
         self.assertEqual(compute2, resultString2)
         self.assertEqual(compute3, resultString3)
+        self.assertEqual(compute4, resultString4)
 
 
 if __name__ == "__main__":
